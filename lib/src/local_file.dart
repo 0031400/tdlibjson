d030d49d29520f733c61cbe494a9446f79d63df2
lxz <liuxiongzhuo@163.com>
2026-02-21 22:54:11 +0800

    添加localfile的isDownloadingActive

--- lib/src/local_file.dart
+++ lib/src/local_file.dart
@@ -2,9 +2,11 @@ class LocalFile {
   late final String path;
   late final bool isDownloadingCompleted;
   late final int downloadedSize;
+  late final bool isDownloadingActive;
   LocalFile.fromJson(Map<String, dynamic> json) {
     path = json['path'];
     isDownloadingCompleted = json['is_downloading_completed'];
     downloadedSize = json['downloaded_size'];
+    isDownloadingActive = json['is_downloading_active'];
   }
 }
