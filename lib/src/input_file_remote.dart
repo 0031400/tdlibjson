1627cee81f304d1343c6a69dfa9482cf5cc5b734
lxz <liuxiongzhuo@163.com>
2026-02-21 23:40:39 +0800

    添加remotefile一些字段

--- lib/src/input_file_remote.dart
+++ lib/src/input_file_remote.dart
@@ -1,5 +1,6 @@
 import 'input_file.dart';
-class InputFileRemote implements InputFile{
+
+class InputFileRemote implements InputFile {
   late final String id;
   InputFileRemote.fromJson(Map<String, dynamic> json) {
     id = json['id'];
--- lib/src/remote_file.dart
+++ lib/src/remote_file.dart
@@ -1,8 +1,14 @@
 class RemoteFile {
   late final String id;
   late final String uniqueId;
+  late final bool isUploadingActive;
+  late final bool isUploadingCompleted;
+  late final int uploadedSize;
   RemoteFile.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     uniqueId = json['unique_id'];
+    isUploadingActive = json['is_uploading_active'];
+    isUploadingCompleted = json['is_uploading_completed'];
+    uploadedSize = json['uploaded_size'];
   }
 }
